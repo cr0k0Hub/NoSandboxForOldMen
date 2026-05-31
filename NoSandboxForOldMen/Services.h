6d0bb7bf4f45e9f306df0ea5055c45b3e76214db
cr0k0Hub <54044201+cr0k0Hub@users.noreply.github.com>
2026-05-31 18:10:54 +0300

    Delete NoSandboxForOldMen/Services.h

--- NoSandboxForOldMen/Services.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <windows.h>
-
-DWORD ServiceTypes[6];
-
-SC_HANDLE GetSCManager();
-BOOL ServiceExist(LPCSTR serviceName);
-BOOL EnumServices(const char* services[], DWORD servicesLength);
